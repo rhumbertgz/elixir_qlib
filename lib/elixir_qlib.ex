bef56cc8fa52b5a8f26c57e4fa1d130534e9b2e4
Humberto Rodríguez Avila <rhumbertgz@gmail.com>
2016-10-06 11:22:20 +0200

    Add general description for documentation

---
+++ lib/elixir_qlib.ex
@@ -0,0 +1,7 @@
+defmodule ElixirQLib do
+  @moduledoc """
+  A simple queue abstraction library to support leasing and buffering in Elixir.
+  QLib provides two main queue abstractions LeasedQueue and BufferedQueue.
+
+  """
+end
